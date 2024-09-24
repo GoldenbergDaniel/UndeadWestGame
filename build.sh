fbbfce8c2dd1ba22ced83a29ba373500e4d14f74
Daniel Goldenberg <danielgoldenberg0@outlook.com>
2024-09-24 17:58:12 -0400

    Start Linux port

--- build.sh
+++ build.sh
@@ -7,18 +7,12 @@ OUT="undeadwest"
 MODE="dev"
 if [[ $1 == "d" || $1 == "debug"   ]]; then MODE="debug"; fi
 if [[ $1 == "r" || $1 == "release" ]]; then MODE="release"; fi
-if [[ $1 == "g" || $1 == "git"     ]]; then MODE="git"; fi
+
+TARGET="darwin_amd64"
+if [[ $1 == "-target" ]]; then TARGET=$2; fi
+if [[ $2 == "-target" ]]; then TARGET=$3; fi
 
 if [[ $1 == "fsan" ]]; then FSAN="-fsanitize=undefined"; fi
-
-if [[ $MODE == "git" ]];
-then
-  git add .
-  git status
-  git commit -m "$2"
-  git push
-  exit 0
-fi
 
 if [[ $MODE == "dev"     ]]; then CFLAGS="-std=c17 -O0"; fi
 if [[ $MODE == "debug"   ]]; then CFLAGS="-std=c17 -Og -g -DDEBUG"; fi
@@ -28,14 +22,15 @@ if [[ $MODE == "dev"     ]]; then WFLAGS="-Wpedantic -Wno-initializer-overrides"
 if [[ $MODE == "debug"   ]]; then WFLAGS="-Wall -Wpedantic -Wno-initializer-overrides -Wno-missing-braces"; fi
 if [[ $MODE == "release" ]]; then WFLAGS="-Wno-initializer-overrides"; fi
 
-LFLAGS="-I../extern/ -L../extern/sokol/lib -lsokol -framework OpenGL -framework Cocoa"
+if [[ $TARGET == "darwin_amd64" ]]; then LFLAGS="-I../extern/ -L../extern/sokol/lib -lsokol -framework OpenGL -framework Cocoa"; fi
+if [[ $TARGET == "linux_amd64"  ]]; then LFLAGS="-I../extern/ -L../extern/sokol/lib -lsokol"; fi
 
 echo "[mode:$MODE]"
 if [[ $2 == "fsan" ]]; then echo "[fsan:on]"; fi
 if [[ $2 != "fsan" ]]; then echo "[fsan:off]"; fi
 
 # echo "- Processing -"
-shadertoh src/shaders/ src/render/shaders.h
+# shadertoh src/shaders/ src/render/shaders.h
 
 BUILD="0"
 if [[ $MODE == "dev" || $MODE == "debug" || $MODE == "release" ]]; then BUILD="1"; fi
